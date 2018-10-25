83aef23e92f00b70d8baee698aafc36dcd221e84
pixelmagenta <eugustinova@gmail.com>
2018-10-25 10:08:13 +0300

    plots added + power law

--- sw_script.R
+++ sw_script.R
@@ -1,14 +1,17 @@
 library("jsonlite")
 library("curl")
 library("igraph")
+library("ggplot2")
 
-plays_to_remove <- list("blok-balaganchik", "blok-korol-na-ploschadi", "blok-neznakomka", "gogol-teatralnyi-razezd")
+corpora <- "ger"
+## plays_to_remove <- list("blok-balaganchik")
+##, "blok-korol-na-ploschadi", "blok-neznakomka", "gogol-teatralnyi-razezd")
 
 ## Downloading plays
-list_of_names <- fromJSON("https://dracor.org/api/corpus/rus")
+list_of_names <- fromJSON(paste0("https://dracor.org/api/corpora/", corpora))
 sorted_ids <- list_of_names$dramas$id[sort.list(list_of_names$dramas$id)]
-sorted_ids <- sorted_ids[sorted_ids != plays_to_remove]  ## removing of plays which do not represent social interactions
-plays <- lapply(sorted_ids, function(x) read.csv(paste0("https://dracor.org/api/corpus/rus/play/", x, "/networkdata/csv"), stringsAsFactors = F))
+## sorted_ids <- sorted_ids[sorted_ids != plays_to_remove]  ## removing of plays which do not represent social interactions
+plays <- lapply(sorted_ids, function(x) read.csv(paste0("https://dracor.org/api/corpora/", corpora, "/play/", x, "/networkdata/csv"), stringsAsFactors = F))
 
 ## Remove 'Type' and 'Weight' variables
 del_vars <- function(play){
@@ -18,9 +21,9 @@ del_vars <- function(play){
 }
 plays <- lapply(plays, del_vars)
 
-metadata <- read.csv("https://dracor.org/api/corpus/rus/metadata.csv", stringsAsFactors = F)
+metadata <- read.csv(paste0("https://dracor.org/api/corpora/", corpora, "/metadata.csv"), stringsAsFactors = F)
 
-metadata <- metadata[metadata$name != plays_to_remove,] ## removing of plays which do not represent social interactions
+## metadata <- metadata[metadata$name != plays_to_remove,] ## removing of plays which do not represent social interactions
 
 ## Creating graphs of plays
 graphs_of_plays <- lapply(plays, function(x) graph_from_data_frame(x, directed = F))
@@ -66,3 +69,56 @@ df$crit_2 <- ifelse((df$crit_1 == TRUE) & (df$APL_dev >= APL_border_min) & (df$A
 
 ## The dataframe with small-world networks
 small_worlds <- na.omit(df[df$crit_2 == TRUE,])
+
+
+## PLOTS ##
+
+theme_set(theme_gray(base_size = 15)) ## size of axis font
+plot1 <- ggplot(na.omit(df), aes(x = year, y = CC_dev, 
+                                 fill = crit_1, shape = crit_2, color = crit_1))+
+  geom_point(size = 6)+
+  scale_shape_manual(values=c(21, 22), name = "Criterion 2")+
+  scale_color_manual(values=c("slateblue3", "indianred3"), name = "Criterion 1")+
+  scale_fill_manual(values=c("slateblue1", "indianred1"), name = "Criterion 1")+
+  labs(x="Year of creation", y = "Clustering coefficient deviation")
+plot1
+
+
+theme_set(theme_gray(base_size = 15)) ## size of axis font
+plot2 <- ggplot(na.omit(df), aes(x = numOfSpeakers, y = CC_dev, 
+                                   fill = crit_1, shape = crit_2, color = crit_1))+
+  geom_point(size = 6)+
+  scale_shape_manual(values=c(21, 22), name = "Criterion 2")+
+  scale_color_manual(values=c("slateblue3", "indianred3"), name = "Criterion 1")+
+  scale_fill_manual(values=c("slateblue1", "indianred1"), name = "Criterion 1")+
+  labs(x="Number of characters", y = "Clustering coefficient deviation")+
+  coord_cartesian(xlim = c(0, 105))+
+  geom_text(aes(label=ifelse(numOfSpeakers>47,as.character(name),'')),hjust=0.5,vjust=1.5, color="black", size=4)
+plot2
+
+
+## Comparison of R-squared for power law ##
+
+## preprocessing
+degree_dist <- lapply(graphs_of_plays, function(x) degree(x, v = V(x), loops = FALSE, normalized = FALSE))
+degree_dist_v <- lapply(degree_dist, as.vector)
+number_of_nodes <- lapply(degree_dist_v, table)
+distribution <- lapply(number_of_nodes, as.data.frame)
+
+## regressions
+fit <- lapply(distribution, function(x) lm(log(as.numeric(x$Freq)) ~ log(as.numeric(x$Var1))))
+df$Rsqrt <- sapply (fit, function(x) summary(x)$r.squared)
+
+## plot for help
+ggplot(data = distribution[[390]], aes(x = as.numeric(Var1), y = as.numeric(Freq)))+ 
+  geom_point(size = 3) + 
+  geom_smooth(method = "lm", formula=y~log(x))+
+  labs(x="Node degree", y = "Number of nodes")
+
+
+
+
+
+
+
+
